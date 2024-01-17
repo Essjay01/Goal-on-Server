fde9d174f6660f98616751aa359ba539bf0f7788
Essjay01 <joboluwaseun28@gmail.com>
2024-01-17 11:11:47 +0100

    created goal manager api

---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config()
+const express = require("express");
+const app = express(); 
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose")
+// const MONGO_URL = 'mongodb+srv://joboluwaseun28:Essjay1@oluwaseun.funmkb9.mongodb.net/?retryWrites=true&w=majority'
+const cors = require("cors");
+const goalRouter = require("./routes/goalRouter")
+
+
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+//routes
+app.get("/", (req, res)=>{
+    res.status(200).json({message: "Welcome to Goals API"})
+});
+app.use("/api/goals",goalRouter)
+//error route
+app.use((req, res)=>{
+    res.status(404).json({message: "Rousource Not Found"})
+})
+
+//db connection
+
+
+const startServer = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {dbName: "goalServer"}) ;
+        app.listen(PORT, ()=>{
+            console.log(`Server running on a port: ${PORT}...`);
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startServer();
